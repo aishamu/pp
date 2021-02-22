3470f8bceb54635fd33e99c8989efddcf2032732
ع م <m@s-MacBook-Pro.local>
2021-02-22 20:52:18 +0300

    Updates

---
+++ precache-manifest.2701ca0dcb9d683381a09b123168b70b.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd7150a058d5151253a00062e22d0ea",
+    "url": "/aishamu/pp.git/index.html"
+  },
+  {
+    "revision": "31db462cc7e1bbd43bf8",
+    "url": "/aishamu/pp.git/static/css/2.397c0040.chunk.css"
+  },
+  {
+    "revision": "4ef2fea4f45eaa9fb53a",
+    "url": "/aishamu/pp.git/static/css/main.94734a7b.chunk.css"
+  },
+  {
+    "revision": "31db462cc7e1bbd43bf8",
+    "url": "/aishamu/pp.git/static/js/2.9b793197.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/aishamu/pp.git/static/js/2.9b793197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ef2fea4f45eaa9fb53a",
+    "url": "/aishamu/pp.git/static/js/main.3fb407e1.chunk.js"
+  },
+  {
+    "revision": "3875ec824e63ae2e7e47",
+    "url": "/aishamu/pp.git/static/js/runtime-main.dff93844.js"
+  },
+  {
+    "revision": "b738fac5d72d675aea5ee4b64bd25b61",
+    "url": "/aishamu/pp.git/static/media/about.b738fac5.jpg"
+  },
+  {
+    "revision": "70e60a5e90de7f4deec2de504a1ffe9f",
+    "url": "/aishamu/pp.git/static/media/blog1.70e60a5e.jpg"
+  },
+  {
+    "revision": "c7f45df67aa638d12e84313af009a4a3",
+    "url": "/aishamu/pp.git/static/media/blog2.c7f45df6.jpg"
+  },
+  {
+    "revision": "c917246c28650098b89ae55d717ad7cc",
+    "url": "/aishamu/pp.git/static/media/blog3.c917246c.jpg"
+  },
+  {
+    "revision": "03783c5172ee1ad128c576bf88fac168",
+    "url": "/aishamu/pp.git/static/media/fa-brands-400.03783c51.eot"
+  },
+  {
+    "revision": "073c2f3ce60eaf69cc2767ef3d989078",
+    "url": "/aishamu/pp.git/static/media/fa-brands-400.073c2f3c.svg"
+  },
+  {
+    "revision": "7559b3774a0625e8ca6c0160f8f6cfd8",
+    "url": "/aishamu/pp.git/static/media/fa-brands-400.7559b377.woff2"
+  },
+  {
+    "revision": "ed2b8bf117160466ba6220a8f1da54a4",
+    "url": "/aishamu/pp.git/static/media/fa-brands-400.ed2b8bf1.ttf"
+  },
+  {
+    "revision": "fe9d62e0d16a333a20e63c3e7595f82e",
+    "url": "/aishamu/pp.git/static/media/fa-brands-400.fe9d62e0.woff"
+  },
+  {
+    "revision": "59215032a4397507b80e5625dc323de3",
+    "url": "/aishamu/pp.git/static/media/fa-regular-400.59215032.ttf"
+  },
+  {
+    "revision": "8fdea4e89ac405d9f8db327adb331d8d",
+    "url": "/aishamu/pp.git/static/media/fa-regular-400.8fdea4e8.svg"
+  },
+  {
+    "revision": "e07d9e40b26048d9abe2ef966cd6e263",
+    "url": "/aishamu/pp.git/static/media/fa-regular-400.e07d9e40.woff2"
+  },
+  {
+    "revision": "e5770f9863963fb576942e25214a226d",
+    "url": "/aishamu/pp.git/static/media/fa-regular-400.e5770f98.woff"
+  },
+  {
+    "revision": "fc9c63c8224fb341fc933641cbdd12ef",
+    "url": "/aishamu/pp.git/static/media/fa-regular-400.fc9c63c8.eot"
+  },
+  {
+    "revision": "4bced7c4c0d61d4f988629bb8ae80b8b",
+    "url": "/aishamu/pp.git/static/media/fa-solid-900.4bced7c4.woff"
+  },
+  {
+    "revision": "acf50f59802f20d8b45220eaae532a1c",
+    "url": "/aishamu/pp.git/static/media/fa-solid-900.acf50f59.ttf"
+  },
+  {
+    "revision": "b557f56e367e59344ca95f9d1fb44352",
+    "url": "/aishamu/pp.git/static/media/fa-solid-900.b557f56e.svg"
+  },
+  {
+    "revision": "b5cf8ae26748570d8fb95a47f46b69e1",
+    "url": "/aishamu/pp.git/static/media/fa-solid-900.b5cf8ae2.woff2"
+  },
+  {
+    "revision": "ef3df98419d143d9617fe163bf4edc0b",
+    "url": "/aishamu/pp.git/static/media/fa-solid-900.ef3df984.eot"
+  },
+  {
+    "revision": "da7ba2a02c60da8ec823e5df2cb67785",
+    "url": "/aishamu/pp.git/static/media/facts_bg.da7ba2a0.jpg"
+  },
+  {
+    "revision": "99e8d1a1799a7a9dc4570f0eebc046f6",
+    "url": "/aishamu/pp.git/static/media/home_bg.99e8d1a1.jpg"
+  }
+]);
