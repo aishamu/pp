3a61840c024a2e1c886231f81b7f360f258c807b
ع م <m@s-MacBook-Pro.local>
2021-03-03 14:39:09 +0300

    /w.

--- src/components/sections/Contact/Contact.js
+++ src/components/sections/Contact/Contact.js
@@ -21,10 +21,49 @@ const contact = () => {
 
             <div class="info-bar">
     <div class="container">
-      <div class="row">
+ 
+      <div class="row" dir='rtl'>
+
+
+
+      <div class="col-md">
+          <div class="email text-center">
+          <h3  style={{float : 'center'}} > <i class='fa fa-wifi	
+'></i></h3>
+            
+      <h5>www.esttishara.com</h5>   
+                        <i  class='fa fa-envelope
+'> infom@esttishara.com</i> 
+                        <br></br>
+                        Esttishara  <i   className='fab fa-twitter'
+                          /> 
+
+<br></br>
+Esttishara      <i   className='fab fa-linkedin'
+                          /> 
+
+
+
+ 
+          </div>
+        </div>
 
         <div class="col-md">
-          <div class="location text-right">
+          <div class="phone text-center">
+          <br></br>
+          <h3  style={{float : 'center'}} > <i class='fa fa-phone'></i></h3>
+             <h5>012-612-5731
+            <br></br>
+              055-673-7325
+              <br></br>
+              ص.ب: 23523-4256
+            </h5>
+          </div>
+        </div>
+
+
+        <div class="col-md">
+          <div class="location text-center">
             <h3  style={{float : 'center'}} > <i class='fas fa-map-marker-alt'></i></h3>
             
             <h5>طريق الأمير سلطان حي النهضة ، مبنى تاج السلطان للأعمال الدور الثاني 
@@ -37,40 +76,8 @@ const contact = () => {
         </div>
  
 
-        <div class="col-md">
-          <div class="phone text-center">
-          <h3  style={{float : 'center'}} > <i class='fa fa-phone'></i></h3>
-             <h5>012-612-5731
-            <br></br>
-              055-673-7325
-              <br></br>
-              ص.ب: 23523-4256
-            </h5>
-          </div>
-        </div>
-
-        <div class="col-md">
-          <div class="email text-center">
-          <h3  style={{float : 'center'}} > <i class='fa fa-wifi	
-'></i></h3>
-            
-      <h5>www.esttishara.com</h5>   
-                        <i  style={{float : 'left', paddingRight : '5px'}} class='fa fa-envelope
-'> infom@esttishara.com</i> 
-                        
-                         <i style={{float : 'left', paddingRight : '5px'}}  className='fab fa-twitter'
-                          /> Esttishara
-
-<br></br>
-                          <i style={{float : 'left', paddingRight : '5px'}}  className='fab fa-linkedin'
-                          /> Esttishara
-
-
-
  
-          </div>
-        </div>
- 
+
 
 
  
