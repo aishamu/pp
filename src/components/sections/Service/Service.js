48d60d4ced8da9a229f303bb0915312800e412db
ع م <m@s-MacBook-Pro.local>
2021-03-08 21:33:48 +0300

    s

--- src/components/sections/Service/Service.js
+++ src/components/sections/Service/Service.js
@@ -41,8 +41,8 @@ const Service = () => {
     <tr>
       <td>  <ul dir='rtl'>
   <li style={{textAlign : 'right'}} > تصميم مؤشرات قياس الاداء</li>
-  <li style={{textAlign : 'right'}} > التخطيط الاستراتيجي و اعداد خطة العمل</li>
-  <li style={{textAlign : 'right'}} > التطوير الاداري و تطوير الاعمال</li>
+  <li style={{textAlign : 'right'}} > التخطيط الاستراتيجي واعداد خطة العمل</li>
+  <li style={{textAlign : 'right'}} > التطوير الاداري وتطوير الاعمال</li>
   <li style={{textAlign : 'right'}} >  حوكمة الشركات</li>
   <li style={{textAlign : 'right'}} >  عمليات اعادة الهيكلة</li>
  
@@ -52,19 +52,19 @@ const Service = () => {
   <li style={{textAlign : 'right'}} >     عمليات صناعة و تحليل القرار</li>
   <li style={{textAlign : 'right'}} >  تصميم الخطط النتعلقة بريادة الاعمال</li>
   <li style={{textAlign : 'right'}} >  تحليل و تطوير الانظمة المتعلقة بسلاسل الامداد</li>
-  <li style={{textAlign : 'right'}} > تطوير و رفع كفاءة ادارة المواد البشرية و ادارتها</li>
-  <li style={{textAlign : 'right'}} > دراسات الجدوى الاقتصادية و تشمل: الدراسات التسويقية - الدرسات الفنية-الدراسات المالية</li>
+  <li style={{textAlign : 'right'}} > تطوير و رفع كفاءة ادارة المواد البشرية وادارتها</li>
+  <li style={{textAlign : 'right'}} > دراسات الجدوى الاقتصادية و تشمل: الدراسات التسويقية - الدرسات الفنية -الدراسات المالية</li>
   
 </ul> </td>
       <td> 
   <ul dir='rtl'>
  
-  <li style={{textAlign : 'right'}} >    تأهيل الشركات للحصول على الايزو في المجالات المختلفة</li>
+  <li style={{textAlign : 'right'}} >تأهيل الشركات للحصول على الايزو في المجالات المختلفة</li>
   <li style={{textAlign : 'right'}} >تصميم عمليات الانتاج و تصميم موقع العمل</li>
   <li style={{textAlign : 'right'}} >  تشخيص و حل المشاكل الصناعية القائمة</li>
   <li style={{textAlign : 'right'}} >تشخيص مشكلات اداء العمليات ووضع الحلول التنفيذية لها</li>
   <li style={{textAlign : 'right'}} >تخطيط العمليا و تبسيط الاجراءات</li>
-  <li style={{textAlign : 'right'}} >حلول التمويل الصناعي و ذلك عن طريق :صندوق التنمية الصناعي- البنوك-المستثمرين</li>
+  <li style={{textAlign : 'right'}} >حلول التمويل الصناعي و ذلك عن طريق : صندوق التنمية الصناعي- البنوك-المستثمرين</li>
   <li style={{textAlign : 'right'}} >اجراءات الدراسات و تحليل البيانات لتطوير بيئة العمل الصناعية</li>
   <li style={{textAlign : 'right'}} > اجراءات الدراسات و تحليل البيانات لتطوير بيئة العمل الصناعية</li>
   <li style={{textAlign : 'right'}} > التأهيل و الاستشارات التشغيلية</li>
